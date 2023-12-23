8a06127c048f56298fce601db5d1803fe9099bae
qsde1 <qsdwy1@gmail.com>
2023-12-23 14:14:47 +0500

    finally 2

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,19 +1,38 @@
 <template>
-    <ion-page>
-        Statistics
+    <ion-page class="ion-justify-content-center">
+      <div class="ion-align-self-center">
+        <ion-grid>
+          <ion-row>
+            <ion-col>поломалось :(</ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
     </ion-page>
   </template>
   
   <script>
   import { 
     IonPage,
+    IonGrid,
+    IonRow,
+    IonCol,
   } from '@ionic/vue';
   
   
   export default {
     components: {
       IonPage,
+      IonGrid,
+      IonRow,
+      IonCol,
     }
   }
   </script>
+
+<style scoped>
+  ion-col{
+    color: white;
+    font-size: 25px;
+  }
+</style>
   
